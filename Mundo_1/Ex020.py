296bc82c9013f5fc0d4f8fa45f47e11b339f7018
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:24:31 -0300

    Update Ex020.py

--- Mundo_1/Ex020.py
+++ Mundo_1/Ex020.py
@@ -1,12 +1,18 @@
-# Um professor quer sortear a ordem de apresentação de trabalhos dos alunos.
-# Faça um programa que leia o nome dos quatros alunos e mostre em ordem sorteada
+""" Desafio 020:
+
+Problema: O mesmo professor do desafio 019 quer sortear a ordem
+          de apresentação de trabalhos dos alunos. Faça um programa
+          que leia o nome dos quatro alunos e mostre a ordem sorteada.
+          
+Resolução do problema:
+"""
 
 import random
 
-al1 = input('Digite o nome do primeiro aluno: ')
-al2 = input('Digite o nome do segundo aluno: ')
-al3 = input('Digite o nome do terceiro aluno: ')
-al4 = input('Digite o nome do quarto aluno: ')
+al1 = input('Digite o nome do primeiro aluno: ').strip().capitalize()
+al2 = input('Digite o nome do segundo aluno: ').strip().capitalize()
+al3 = input('Digite o nome do terceiro aluno: ').strip().capitalize()
+al4 = input('Digite o nome do quarto aluno: ').strip().capitalize()
 alunos = [al1,al2,al3,al4]
 random.shuffle(alunos)
 print('A ordem de apresentação das equipes vai ser: {}'.format(alunos))
