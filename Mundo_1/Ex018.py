c0787a6a94b0f4a2ea5472bc2018f2bb8cc37f4c
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:21:06 -0300

    Update Ex018.py

--- Mundo_1/Ex018.py
+++ Mundo_1/Ex018.py
@@ -6,8 +6,8 @@ Problema: Faça um programa que leia um ângulo qualquer e mostre na
 Resolução do problema:
 """
 
-import math
-from math import cos, sin, tan
+from math import cos, sin, tan, radians
 
 ang = float(input('Digite o valor de um ângulo qualquer: '))
-print('Analisando o ângulo de {}°, temos que: \nSeno: {:.2f} \nCosseno: {:.2f} \nTangente: {:.2f}'.format(ang, sin(math.radians(ang)), cos(math.radians(ang)), tan(math.radians(ang))))
+print('Analisando o ângulo de {}°, temos que: \nSeno: {:.2f} \nCosseno: {:.2f} \nTangente: {:.2f}'.format(ang, sin(radians(ang)), cos(radians(ang)), tan(radians(ang))))
+
