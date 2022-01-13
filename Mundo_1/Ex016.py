4600121d94dd2918e3bfd8cea94aad33e801a260
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:16:32 -0300

    Update Ex016.py

--- Mundo_1/Ex016.py
+++ Mundo_1/Ex016.py
@@ -1,8 +1,14 @@
-#  Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+""" Desafio 016:
+
+Problema: Crie um programa que leia um número Real qualquer
+          pelo teclado e mostre na tela a sua porção Inteira.
+          
+Resolução do problema:
+"""
 
 # from math import trunc
 # num = float(input('Digite um número com várias casas decimais: '))
 # print('A porção inteira do número {} é {}'.format(num,trunc(num)))
 
-num = float(input('Digite um número: '))
+num = float(input('Digite um número flutuante: '))
 print('A porção inteira do número {} é {}'.format(num,int(num)))
