5f309b67a9f5d572e6c17ea85c4b65a98df071f2
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:15:14 -0300

    Update Ex015.py

--- Mundo_1/Ex015.py
+++ Mundo_1/Ex015.py
@@ -1,5 +1,12 @@
-# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado.
-# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+""" Desafio 015:
+
+Problema: Escreva um programa que pergunte a quantidade de Km
+          percorridos por um carro alugado e a quantidade de dias
+          pelos quais ele foi alugado. Calcule o preço a pagar,
+          sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
+          
+Resolução do problema:
+"""
 
 cliente = input('Nome do cliente que alugou o carro: ')
 dias = int(input('Por quantos dias o cliente alugou o carro: '))
