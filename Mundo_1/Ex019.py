b34b2a0c3729bd38af2186feb94886d6d27dc205
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:21:36 -0300

    Update Ex019.py

--- Mundo_1/Ex019.py
+++ Mundo_1/Ex019.py
@@ -1,5 +1,11 @@
-# Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
-# Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+""" Desafio 019:
+
+Problema: Um professor quer sortear um dos seus quatro alunos para
+          apagar o quadro. Faça um programa que ajude ele, lendo o
+          nome dos alunos e escrevendo na tela o nome do escolhido.
+          
+Resolução do problema:
+"""
 
 import random
 
