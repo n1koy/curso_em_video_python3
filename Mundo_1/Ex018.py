859639ae2b7fe1168ec50b4f31a849cbd80d3f76
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:19:47 -0300

    Update Ex018.py

--- Mundo_1/Ex018.py
+++ Mundo_1/Ex018.py
@@ -7,7 +7,7 @@ Resolução do problema:
 """
 
 import math
-# from math import cos, sin, tan
+from math import cos, sin, tan
 
 ang = float(input('Digite o valor de um ângulo qualquer: '))
 print('Analisando o ângulo de {}°, temos que: \nSeno: {:.2f} \nCosseno: {:.2f} \nTangente: {:.2f}'.format(ang, sin(math.radians(ang)), cos(math.radians(ang)), tan(math.radians(ang))))
