f875433bde0c958e8bcf4b00f23f4863a95a748e
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 09:59:05 -0300

    Update Ex008.py

--- Mundo_1/Ex008.py
+++ Mundo_1/Ex008.py
@@ -6,9 +6,9 @@ Problema: Desenvolva um programa que leia as três notas de um aluno,
 Resolução do problema:
 """
 
-al = input('Digite o nome do aluno: ')
+al = input('Digite o nome do aluno: ').strip()
 n1 = float(input('Digite a nota da primeira prova: '))
 n2 = float(input('Digite a nota da segunda prova: '))
 n3 = float(input('Digite a nota da terceira prova: '))
 me = (n1+n2+n3) / 3
-print('A média das notas do aluno "{}" foi de {:.2f} pontos'.format(al,me))
+print('A média das notas do aluno "{}" foi de {:.2f} pontos'.format(al.capitalize(),me))
