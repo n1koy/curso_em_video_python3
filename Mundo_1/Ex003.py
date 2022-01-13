ef1727742896f82edfdbe49de84a2327a54e9851
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 09:46:23 -0300

    Add files via upload

---
+++ Mundo_1/Ex003.py
@@ -0,0 +1,11 @@
+""" Desafio 003:
+
+Problema: Crie um programa que leia dois números e mostre a soma entre eles.
+
+Resolução do problema:
+"""
+
+n1 = int(input('\033[1;31mDigite um valor: '))
+n2 = int(input('Digite outro valor: \033[m'))
+soma = n1+n2
+print('\033[35mA soma entre \033[4;33m{} e {}\033[m \033[35mvale \033[4;36m{}\033[m.'.format(n1,n2,soma))
