59a28915c09755cdb4a1bdb67847337ae4ee0f2f
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 10:14:53 -0300

    Update Ex014.py

--- Mundo_1/Ex014.py
+++ Mundo_1/Ex014.py
@@ -1,4 +1,10 @@
-# Converter temperatura em celsius para fahrenheit
+""" Desafio 014:
+
+Problema: Escreva um programa que converta uma temperatura
+          digitando em graus Celsius e converta para graus Fahrenheit.
+          
+Resolução do problema:
+"""
 
 celsius = float(input('Informe a temperatura em °C: '))
 fah = (celsius*9/5)+32
