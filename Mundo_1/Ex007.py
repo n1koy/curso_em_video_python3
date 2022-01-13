f6fe2504f6ac72dda3d8e3996dd4af4a20f9db4c
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 09:58:04 -0300

    Update and rename Ex007.py to Ex008.py

--- Mundo_1/Ex007.py
+++
@@ -1,8 +0,0 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
-
-al = input('Digite o nome do aluno: ')
-n1 = float(input('Digite a nota da primeira prova: '))
-n2 = float(input('Digite a nota da segunda prova: '))
-n3 = float(input('Digite a nota da terceira prova: '))
-me = (n1+n2+n3) /3
-print('A média das notas do aluno "{}" foi de {:.2f} pontos'.format(al,me))
