a75430d4c08a02699a027c848a484f3fa9e1ca51
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 09:40:37 -0300

    Update and rename boas-vindas.py to Ex002.py

---
+++ Mundo_1/Ex002.py
@@ -0,0 +1,10 @@
+""" Desafio 002:
+
+Problema: Faça um programa que leia o nome de uma pessoa e mostre
+          uma mensagem de boas-vindas.
+          
+Resolução do problema:
+"""
+
+nome = input('Qual o seu nome?')
+print('É um prazer em lhe conhecer, {}!'.format(nome))
