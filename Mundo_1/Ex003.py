91cff5b306d18dc414b5fa3ba99f9337ab403541
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-13 09:47:53 -0300

    Update Ex003.py

--- Mundo_1/Ex003.py
+++ Mundo_1/Ex003.py
@@ -6,6 +6,6 @@ Resolução do problema:
 """
 
 n1 = int(input('\033[1;31mDigite um valor: '))
-n2 = int(input('Digite outro valor: \033[m'))
+n2 = int(input('Digite outro valor: '))
 soma = n1+n2
-print('\033[35mA soma entre \033[4;33m{} e {}\033[m \033[35mvale \033[4;36m{}\033[m.'.format(n1,n2,soma))
+print('\033[0;35mA soma entre \033[4;33m{} e {}\033[m \033[35mvale \033[4;36m{}'.format(n1,n2,soma))
