180435d0d7a62be4ef148013fb555042fd57450e
n1koy <96537896+n1koy@users.noreply.github.com>
2022-01-15 11:04:49 -0300

    Update Ex028.py

--- Mundo_1/Ex028.py
+++ Mundo_1/Ex028.py
@@ -1,6 +1,11 @@
-# Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5
-# E peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
-# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+''' Desafio 28:
+
+Problema: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5
+          E peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+          O programa deverá escrever na tela se o usuário venceu ou perdeu.
+          
+Resolução:
+'''
 
 import random
 import time
